37cec852bb9051f77384fc9dcfc1126180978b70
Nathan Booth <boothnj@gmail.com>
2019-02-05 17:09:29 -0600

    Add page crawling to spider

--- data_science_intro/web_scraping/scrapy/quotes_spider/quotes_spider/spiders/quotes1.py
+++ data_science_intro/web_scraping/scrapy/quotes_spider/quotes_spider/spiders/quotes1.py
@@ -13,3 +13,17 @@ class QuotesSpider(scrapy.Spider):
         quotes = response.xpath('//*[@class="quote"]')
 
         for quote in quotes:
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quote.xpath('.//*[@class="author"]/text()').extract_first()
+            tags = quote.xpath('.//*[@class="keywords"]/@content').extract_first()
+            # similar but returns a list
+            # quote.xpath('.//*[@class="tag"]/text()').extract()
+
+            # print(text, "\n", author, "\n", tags, "\n\n")
+            yield {'text': text,
+                   'author': author,
+                   'tags': tags}
+
+        next_page_relative = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        next_page_absolute = response.urljoin(next_page_relative)
+        yield scrapy.Request(next_page_absolute)
