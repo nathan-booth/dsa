9512fa84c385abd1a923ee4334bc981a29b2b0d6
Nathan <boothnj@gmail.com>
2019-03-27 09:43:20 -0400

    Add date, url data to spider

--- data_science_intro/web_scraping/scrapy/craigslist/craigslist/spiders/jobs.py
+++ data_science_intro/web_scraping/scrapy/craigslist/craigslist/spiders/jobs.py
@@ -8,6 +8,17 @@ class JobsSpider(scrapy.Spider):
     start_urls = ['https://washingtondc.craigslist.org/d/software-qa-dba-etc/search/nva/sof']
 
     def parse(self, response):
-        titles = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
-        for title in titles:
-            yield {'Title': title}
+        listings = response.xpath('//li[@class="result-row"]')
+        for listing in listings:
+            date = listing.xpath('.//time[@class="result-date"]/@datetime').extract_first()
+            title = listing.xpath('.//*[@class="result-title hdrlnk"]/text()').extract_first()
+            url = listing.xpath('.//*[@class="result-title hdrlnk"]/@href').extract_first()
+
+            yield {'date': date,
+                   'title': title,
+                   'url': url}
+
+        next_page_url = response.xpath('//a[text()="next > "]/@href').extract_first()
+        if next_page_url:
+            yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
+
